ce064c7a684ef1889556916053f25238d55755f7
Michel Schanen <mschanen@anl.gov>
2022-06-12 14:48:17 -0500

    Scaling code

--- scaling/scaling.jl
+++ scaling/scaling.jl
@@ -4,6 +4,11 @@
 # file. PProf has to be installed in the global environment.
 #############################################################################
 
+using OpenBLAS_jll
+using Libdl
+# Libdl.dlopen("/sw/summit/spack-envs/base/opt/linux-rhel8-ppc64le/gcc-8.3.1/openblas-0.3.17-y7jcbk7ngph55fl62dk2xe3lds4dz5v4/lib/libopenblas.so", RTLD_NOW | RTLD_GLOBAL ;throw_error=true)
+Libdl.dlopen(OpenBLAS_jll.libopenblas_path, RTLD_NOW | RTLD_GLOBAL ;throw_error=true)
+Libdl.dlopen(OpenBLAS_jll.libopenblas_path, RTLD_NOW | RTLD_GLOBAL ;throw_error=true)
 using ProxAL
 #using DelimitedFiles, Printf
 using LinearAlgebra, JuMP, Ipopt
@@ -12,21 +17,16 @@ using LinearAlgebra, JuMP, Ipopt
 using MPI
 using Logging
 using LazyArtifacts
-using Libdl
 
-Libdl.dlopen("/sw/summit/spack-envs/base/opt/linux-rhel8-ppc64le/gcc-8.3.1/openblas-0.3.17-y7jcbk7ngph55fl62dk2xe3lds4dz5v4/lib/libopenblas.so", RTLD_NOW | RTLD_GLOBAL ;throw_error=true)
 
 MPI.Init()
 
-# case = "case9"
 case = "case_ACTIVSg10k"
-demandfiles = "$(case)_oneweek_168"
-# Large 2000 bus case
-# demandfiles = "$(case)"
+demandfiles = "case_ACTIVSg10k_Jun_oneweek_1008_10min"
 
 # choose one of the following (K*T subproblems in each case)
 if length(ARGS) == 0
-    (T, K) = (1, 0)
+    (T, K) = (4, 1)
 elseif length(ARGS) == 4
     case = ARGS[1]
     demandfiles = ARGS[2]
@@ -46,9 +46,9 @@ backend = ProxAL.AdmmBackend()
 
 
 # Load case
-const DATA_DIR = joinpath(artifact"ExaData", "ExaData", "matpower")
+const DATA_DIR = joinpath(@__DIR__, "..", "data")
 case_file = joinpath(DATA_DIR, "$(case).m")
-load_file = joinpath(@__DIR__, "..", "data", "ACTIVSg_Time_Series", "mp_demand", "case_ACTIVSg10k_Jun_oneweek_1008_10min")
+load_file = joinpath(@__DIR__, "..", "data", demandfiles)
 
 # Model/formulation settings
 modelinfo = ModelInfo()
@@ -76,18 +76,18 @@ if isa(backend, ProxAL.AdmmBackend)
     algparams.device = ProxAL.CUDADevice
 end
 #algparams.optimizer = optimizer_with_attributes(
-#    Ipopt.Optimizer, 
+#    Ipopt.Optimizer,
 #    "print_level" => 0,
 #    "linear_solver" => "ma27"
 #) #,  "tol" => 1e-1*algparams.tol)
-algparams.tron_rho_pq=3*1e4
-algparams.tron_rho_pa=3*1e5
+algparams.tron_rho_pq=3*1e3
+algparams.tron_rho_pa=3*1e4
 algparams.tron_outer_iterlim=10
 algparams.tron_inner_iterlim=500
 algparams.tron_scale=1e-5
 algparams.mode = :coldstart
 algparams.init_opf = false
-algparams.verbose_inner=1
+algparams.verbose_inner=0
 algparams.tron_outer_eps = 2e-4
 
 
